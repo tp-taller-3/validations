3c7732335ed05458d0942690f6a29e1583869e4d
Sebastian Blanco <sblanco@devartis.com>
2020-02-04 02:53:47 -0300

    Use max length error variable

--- src/validateName.ts
+++ src/validateName.ts
@@ -2,7 +2,7 @@ import { EmptyNameError, LongNameError, NameWithDigitsError } from "./Errors";
 
 const validateName = name => {
   if (name.length === 0) throw new EmptyNameError();
-  if (name.length >= 35) throw new LongNameError();
+  if (name.length >= LongNameError.maximumNameLength) throw new LongNameError();
   if (/\d/.test(name)) throw new NameWithDigitsError();
   return true;
 };
