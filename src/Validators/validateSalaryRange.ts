c8a50ea93149ffd69e9a1195fac5cf28adcc1589
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-04-07 21:55:59 -0300

    Remove unnecessary new line

--- src/Validators/validateSalaryRange.ts
+++ src/Validators/validateSalaryRange.ts
@@ -2,7 +2,6 @@ import { SalaryRangeError } from "../Errors";
 
 const validateSalaryRange = (minimumSalary: number, maximumSalary: number) => {
   if (minimumSalary > maximumSalary) throw new SalaryRangeError();
-
 };
 
 export { validateSalaryRange };
