1d1f1c0e40e30c8833c365dd602b1db5d54b6de0
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-05-08 18:05:04 -0300

    Change InvalidPhoneNumberError Message

--- src/Errors/InvalidPhoneNumberError.ts
+++ src/Errors/InvalidPhoneNumberError.ts
@@ -1,6 +1,6 @@
 export class InvalidPhoneNumberError extends Error {
   public static buildMessage() {
-    return "Número de telefono inválido. Ejemplo: 11 2939 0394";
+    return "Número de telefono inválido. Ejemplos: 11 6029 2354, 11 4777 3424";
   }
 
   constructor() {
