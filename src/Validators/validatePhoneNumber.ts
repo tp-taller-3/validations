bc787daf52fc8594045c5fe7fdd8028433e661af
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-05-08 19:00:26 -0300

    Add more specific phone number test

--- src/Validators/validatePhoneNumber.ts
+++ src/Validators/validatePhoneNumber.ts
@@ -6,20 +6,24 @@ interface IValidatePhoneNumberArguments {
   countryCode?: CountryCode;
 }
 
-const validatePhoneNumber = (
+const validatePhoneNumberWithCountryCode = (
   {
     phoneNumber,
-    countryCode = "AR"
-  }: IValidatePhoneNumberArguments
-) => {
-  if (phoneNumber.match(/[a-z]/i)) throw new PhoneNumberWithLettersError();
-
-  const parsedPhoneNumber = parsePhoneNumberFromString(
-    phoneNumber,
-    { defaultCountry: countryCode }
-  );
+    countryCode
+  }: IValidatePhoneNumberArguments) => {
+  const parsedPhoneNumber = parsePhoneNumberFromString(phoneNumber, countryCode);
   if (parsedPhoneNumber && parsedPhoneNumber.isPossible() && parsedPhoneNumber.isValid()) return;
   throw new InvalidPhoneNumberError();
+};
+
+const validatePhoneNumber = (phoneNumber: string) => {
+  if (phoneNumber.match(/[a-z]/i)) throw new PhoneNumberWithLettersError();
+
+  try {
+    validatePhoneNumberWithCountryCode({ phoneNumber });
+  } catch (error) {
+    validatePhoneNumberWithCountryCode({ phoneNumber, countryCode: "AR" });
+  }
 };
 
 export { validatePhoneNumber };
