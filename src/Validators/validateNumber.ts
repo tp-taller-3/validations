3731b024f6373779fb371cfe3a11542d1086f881
Dylan Alvarez <dalvarez@devartis.com>
2020-11-13 03:00:15 -0300

    Test that all numbers fit into 32 bits so that they can be transmitted through a GraphQLInt

--- src/Validators/validateNumber.ts
+++ src/Validators/validateNumber.ts
@@ -1,7 +1,15 @@
-import { NumberIsNaNError } from "../Errors";
+import { NumberIsNaNError, NumberIsTooLargeError, NumberIsTooSmallError } from "../Errors";
+
+const MAX_32_BIT_INTEGER = Math.pow(2, 32) / 2 - 1;
 
 const validateNumber = (aNumber: number) => {
   if (Number.isNaN(aNumber)) throw new NumberIsNaNError();
+  if (Math.abs(aNumber) > MAX_32_BIT_INTEGER) {
+    throw new NumberIsTooLargeError(MAX_32_BIT_INTEGER, true);
+  }
+  if (Math.abs(aNumber) < -MAX_32_BIT_INTEGER) {
+    throw new NumberIsTooSmallError(-MAX_32_BIT_INTEGER, true);
+  }
 };
 
 export { validateNumber };
