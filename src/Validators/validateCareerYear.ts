e92ad475edfa12b409d1ee58c509f56162ed483e
Dylan Alvarez <dalvarez@devartis.com>
2020-11-15 16:43:02 -0300

    Make validate career year a constant instead of a function

--- src/Validators/validateCareerYear.ts
+++ src/Validators/validateCareerYear.ts
@@ -1,4 +1,4 @@
 import { validateIntegerInRange } from "./validateIntegerInRange";
 
-export const validateCareerYear = () =>
+export const validateCareerYear =
   validateIntegerInRange({min: {value: 1, include: true}, max: {value: 5, include: true}});
