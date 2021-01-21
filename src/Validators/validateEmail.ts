54baabb2d48c8970b627d88ff437fd7888a075eb
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2021-01-20 21:39:15 -0300

    Export validateEmail inline

--- src/Validators/validateEmail.ts
+++ src/Validators/validateEmail.ts
@@ -1,8 +1,6 @@
 import isEmail from "validator/lib/isEmail";
 import { InvalidEmailError } from "../Errors";
 
-const validateEmail = (email: string) => {
+export const validateEmail = (email: string) => {
   if (!isEmail(email)) throw new InvalidEmailError(email);
 };
-
-export { validateEmail };
