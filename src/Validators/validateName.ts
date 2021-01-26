628eecbfdb53186b3aa44af8b5e6f8e1793745d2
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2021-01-26 00:32:59 -0300

    Throw error if the name has more than a hundred caracteres

--- src/Validators/validateName.ts
+++ src/Validators/validateName.ts
@@ -1,8 +1,9 @@
-import { EmptyNameError, NameWithDigitsError } from "../Errors";
+import { EmptyNameError, NameWithDigitsError, NameIsTooLargeError } from "../Errors";
 
 const validateName = name => {
   if (name.length === 0) throw new EmptyNameError();
   if (/\d/.test(name)) throw new NameWithDigitsError();
+  if (name.length > 100) throw new NameIsTooLargeError(100);
 };
 
 export { validateName };
