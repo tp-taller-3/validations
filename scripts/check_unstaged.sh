29b8ae4e8e5f27bf3a034cb296f40e5bf08946ad
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2021-01-26 00:34:42 -0300

    Fix check_unstaged script print message

--- scripts/check_unstaged.sh
+++ scripts/check_unstaged.sh
@@ -3,7 +3,7 @@
 if [[ $(git ls-files --others --modified --exclude-standard) ]]; then
   echo ""
   echo "ERROR: There are unstaged changes."
-  echo "  Run 'yarn stash', commit, and then run 'yarn unstash'."
+  echo "  Run 'npm run stash', commit, and then run 'npm run unstash'."
   echo ""
   exit 1
 fi
